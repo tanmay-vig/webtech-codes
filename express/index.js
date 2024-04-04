3e9b5f35de0a16f03e503924765ccd21adb40cda
Tanmay Vig <110380506+tanmay-vig@users.noreply.github.com>
2024-04-04 18:16:43 +0000

    ending-responses

--- express/index.js
+++ express/index.js
@@ -4,14 +4,17 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  res.end();
 });
 
 app.get("/api/courses", (req, res) => {
     res.send([1, 2, 3]);
+    res.end();
     });
 
 app.get("/api/courses/:id", (req, res) => {
     res.send(req.params.id);
+    res.end();
     } );
 
 app.listen(3000, () => {
